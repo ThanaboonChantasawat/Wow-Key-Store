51e6c2ae695970b624648d4865e3f8406237894e
Thanaboon Chantasawat <mystic28k@gmail.com>
2025-08-08 00:03:17 +0700

    Remove unused "use client" directive from seller profile page

--- app/sellerprofile/[id]/page.tsx
+++ app/sellerprofile/[id]/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Star, ShoppingCart, User } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
